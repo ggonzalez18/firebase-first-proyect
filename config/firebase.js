dad3169bd1bc899a52c4412c4f1d4e159c619732
Gabriela Gonzalez <gabytasmile7@gmail.com>
2020-08-27 19:02:19 -0400

    iniciando primer proyecto con firebase

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+ export default {
+    apiKey: "AIzaSyC6qacnBC4BiMGx56vYFESs-U5GFiSlPvg",
+    authDomain: "myfirstproyect-8aa5b.firebaseapp.com",
+    databaseURL: "https://myfirstproyect-8aa5b.firebaseio.com",
+    projectId: "myfirstproyect-8aa5b",
+    storageBucket: "myfirstproyect-8aa5b.appspot.com",
+    messagingSenderId: "378127547909",
+    appId: "1:378127547909:web:904068e40d845455e9e66c"
+  }
