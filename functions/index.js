f1596839c8460673f6f4aba851626223ec792712
Gabriela Gonzalez <gabytasmile7@gmail.com>
2020-08-28 21:55:07 -0400

    creamos api en firebase, la conectamos y visualizamos en el admin el name

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+const functions = require('firebase-functions');//servidor node
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase);
+const express = require('express');
+const cors = require('cors'); // se usa para acceder a otro dominio
+const app = express();// Automatically allow cross-origin requestsrouter.use(cors({ origin: true }));
+app.use(cors({ origin: true }))
+
+//end point express
+app.get('/beers', async (req, rest) => {
+  const beers = await admin.firestore().collection('beers').get()//va a buscar toda la coleccion de cervezas
+  const list = []
+  beers.docs.forEach(doc => {
+    list.push({id: doc.id, data: doc.data()})
+  })
+  rest.send(list)
+})
+
+app.get('/beers/:id', async (req, res) => {
+  const beer = await admin.firestore().collection('beers')
+    .doc(req.params.id).get().then((doc) => {
+      if (doc.exists) {
+        return {
+          id: doc.id, data: doc.data()
+        }
+      } else {
+        return {}
+      }
+    })
+  res.send(beer)
+})
+
+exports.api = functions.https.onRequest(app)
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="container">
     <h1>Bienvenido {{ currentUser.email}}</h1>
+    <div>
+      <ul>
+        <li v-for="beer in beers" :key="beer.id">{{beer.name}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import {mapState, mapActions} from 'vuex'
 
 export default {
   computed: {
-    ...mapState(['currentUser'])
+    ...mapState(['currentUser', 'beers'])
+  },
+  methods: {
+    ...mapActions(['setBeers'])
+  },
+  created() {
+    this.setBeers
   }
 }
 </script>
